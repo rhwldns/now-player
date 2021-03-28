7094645d27624e0ff6a002eac54b61a08ce069f6
peanut <jjun4341@naver.com>
2021-03-28 20:45:43 +0900

    feat: make playlist and name

--- now.py
+++ now.py
@@ -9,9 +9,12 @@ root.resizable(False, False)
 def change():
     a = Toplevel(root)
     a.title('Set Playlist Name')
-    a.geometry('640x480')
-    txt = Text(a)
+    a.geometry('320x100')
+    e = Label(a, text = '아래에 플레이리스트 이름을 작성하세요.')
+    e.pack()
+    txt = Text(a, width=50, height=1)
     txt.pack()
+    
 
 bt = Button(root, padx=10, pady=5, text='Make Playlist', command=change)
 bt.pack()
