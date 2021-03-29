4bba3fe76fce6bcaef40ca8b80f37273b92ef83c
peanut <jjun4341@naver.com>
2021-03-29 19:33:38 +0900

    feat: Make File(playlist)

--- now.py
+++ now.py
@@ -9,7 +9,7 @@ from tkinter import *
 root = Tk()
 root.title("Now Music Player")
 root.geometry("640x480")
-
+root.iconphoto(False, PhotoImage(file='nowico.ico'))
 root.resizable(False, False)
 
 def make_pl():
@@ -24,15 +24,23 @@ def make_pl():
     e.pack()
     def btncmd():
         aa = e.get()
-        print(aa)
+        with open(f'{aa}.txt', 'a', encoding='UTF-8') as f:
+            f.write('.')
+            f.seek(0)
+            f.truncate()
         a.destroy()
+        c = Toplevel(root)
+        c.geometry('100x50')
+        tt = Label(c, text = '플레이리스트 제작이 완료되었습니다.')
     
     b = Button(a, padx=10, pady=5, text='완료', command=btncmd)
     b.pack()
     
     
-
-bt = Button(root, padx=10, pady=5, text='Make Playlist', command=make_pl)
+def play():
+    print(1)
+bt = Button(root, padx=10, pady=5, text='플레이리스트 제작', command=make_pl)
+b = Button(root, padx=10, pady=5, text='재생', command=play)
 bt.pack()
 
 root.mainloop()
