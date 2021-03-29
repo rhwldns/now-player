8db185cf0e53542e26bbe061797b5addf683ed54
peanut <jjun4341@naver.com>
2021-03-29 18:17:12 +0900

    feat: playlist make

--- now.py
+++ now.py
@@ -1,22 +1,38 @@
 from tkinter import *
 
+##########################################################
+# 단축 용어 정리
+
+# pl = 플레이리스트 (playlist)
+
+##########################################################
 root = Tk()
 root.title("Now Music Player")
 root.geometry("640x480")
 
 root.resizable(False, False)
 
-def change():
+def make_pl():
     a = Toplevel(root)
     a.title('Set Playlist Name')
     a.geometry('320x100')
-    e = Label(a, text = '아래에 플레이리스트 이름을 작성하세요.')
+
+
+    t = Label(a, text = '아래에 플레이리스트 이름을 작성하세요.')
+    t.pack()
+    e = Entry(a, width=10)
     e.pack()
-    txt = Text(a, width=50, height=1)
-    txt.pack()
+    def btncmd():
+        aa = e.get()
+        print(aa)
+        a.destroy()
+    
+    b = Button(a, padx=10, pady=5, text='완료', command=btncmd)
+    b.pack()
+    
     
 
-bt = Button(root, padx=10, pady=5, text='Make Playlist', command=change)
+bt = Button(root, padx=10, pady=5, text='Make Playlist', command=make_pl)
 bt.pack()
 
 root.mainloop()
