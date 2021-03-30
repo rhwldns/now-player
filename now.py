e18f01ae72931d682c27b618059ef1dcedb99a31
peanut <jjun4341@naver.com>
2021-03-30 21:50:50 +0900

    feat: search

--- now.py
+++ now.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import os
 import os.path
+import requests
+from youtubesearchpython import VideosSearch
 
 ##########################################################
 # 단축 용어 정리
@@ -20,7 +22,7 @@ def make_pl():
     a.geometry('320x100')
 
 
-    t = Label(a, text = '아래에 플레이리스트 이름을 작성하세요.')
+    t = Label(a, text = '아래에 플레이리스트 이름을 작성하세요.\n특수문자는 사용이 불가능합니다.')
     t.pack()
     e = Entry(a, width=10)
     e.pack()
@@ -43,9 +45,6 @@ def make_pl():
     b = Button(a, padx=10, pady=5, text='완료', command=btncmd)
     b.pack()
     
-    
-def play():
-    print(1)
 
 def del_pl(): 
     a = Toplevel(root)
@@ -80,12 +79,70 @@ def del_pl():
     b = Button(a, padx=10, pady=5, text='완료', command=delpl)
     b.pack()
 
-            
+def play():
+    a = Toplevel(root)
+    z = ''
+    for currentdir, dirs, files in os.walk("playlist/"):
+    
+        for file in files :
+            file = file.replace('.txt', '')
+            z += file + '\n'
+    pl_list = Label(a, text = f'플레이리스트 목록입니다.\n재생할 플레이리스트 이름을 입력해주세요.\n\n{z}')
+    pl_list.pack()
+    e = Entry(a, width=10)
+    e.pack()
 
-bt = Button(root, padx=10, pady=5, text='플레이리스트 제작', command=make_pl)
+    def playpl():
+
+        aa = e.get()
+        if os.path.isfile(f'playlist/{aa}.txt'):
+
+            # 파일 읽고 그거 다운 후 재생
+
+            with open(f'playlist/{aa}.txt', 'r+', encoding='UTF-8') as f:
+                r = f.readlines()
+                for i in r:
+                    pass
+
+        else:
+            c = Toplevel(a)
+            c.geometry('250x70')
+            tt = Label(c, text = '플레이리스트를 찾을 수 없습니다.\n오타가 있는지 확인해주세요.')
+            tt.pack()
+
+    b = Button(a, padx=10, pady=5, text='완료', command=playpl)
+    b.pack()
+
+def search():
+    a = Toplevel(root)
+    a.title('Set Playlist Name')
+    a.geometry('320x100')
+
+
+    t = Label(a, text = '아래에 검색할 곡의 이름을 작성하세요.')
+    t.pack()
+    e = Entry(a, width=10)
+    e.pack()
+    def bncmd():
+        sn = e.get()
+
+    
+        vs = VideosSearch(f'IU {sn}', limit = 1)
+        vs = dict(vs.result())
+        url = vs['result']['descriptionSnippet']['channel']['link']
+        print(str(url))
+    b = Button(a, padx=10, pady=5, text='완료', command=bncmd)
+    b.pack()
+
+    
+
+
+makepl = Button(root, padx=10, pady=5, text='플레이리스트 제작', command=make_pl)
 del_pl = Button(root, padx=10, pady=5, text='플레이리스트 삭제', command=del_pl)
-b = Button(root, padx=10, pady=5, text='재생', command=play)
-bt.pack()
+playsong = Button(root, padx=10, pady=5, text='재생', command=play)
+search = Button(root, padx=10, pady=5, text='검색', command=search)
+makepl.pack()
 del_pl.pack()
+search.pack()
 
 root.mainloop()
---
+++ test.py
@@ -0,0 +1,5 @@
+from youtubesearchpython import VideosSearch
+
+videosSearch = VideosSearch('NoCopyrightSounds', limit = 2)
+
+print(videosSearch.result())
